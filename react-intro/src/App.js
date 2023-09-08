dbec22ee1f3cd1e375be9be1b13462615e870b08
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2023-09-08 10:43:51 +0300

    Добавление нового компонента Просмотр проппсов, с работой компонента НЕЛЬЗЯ! Компоненты смешивать, как показано, должен быть отдельный файл

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+const Greetings = (props) => <div>Hey you! {props.firstName} {props.lastName}!</div>;
+const GreetingsPro = ({ firstName, lastName }) => <div>Hey you! {firstName} {lastName}!</div>;
 
 function App() {
   return (
@@ -18,6 +20,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <Greetings firstName="Stas" lastName="Oskol" />
+        <GreetingsPro firstName="Stas" lastName="Oskol" />
+      </main>
     </div>
   );
 }
