7c76c5e3e6dc75a44f9472332c6aebfeecd23549
Stas Osk <stas-stas.stanislav@mail.ru>
2025-07-01 03:38:46 +0300

    удаление элемента товара, если количество равно 0, и ликвидация бага - количество товаров не может быть меньше 0

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -7,13 +7,21 @@ interface CartItemProps {
 }
 
 const CartItem = ({ item, onUpdateQuantity, onRemove }: CartItemProps) => { //* Добавил пропс и callback-функции
+    const changeQuantity = (quantity: number) => {
+        if (quantity > 0) {
+            onUpdateQuantity(quantity);
+        } else if (quantity === 0) {
+            onRemove();
+        }
+    }
+
     return (
         <div>
             <span>{item.name}</span>
             <input
                 type="number"
                 value={item.quantity}
-                onChange={(e) => onUpdateQuantity(parseInt(e.target.value))}
+                onChange={(e) => changeQuantity(parseInt(e.target.value))}
             />
             <span>{item.price} ₽</span>
             <button onClick={onRemove}>Удалить</button>
