a38fb86dc776a866fb1997541f6fa85b95320e8a
Stas Osk <stas-stas.stanislav@mail.ru>
2025-07-01 04:49:44 +0300

    реализация работы через mobx

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,58 +1,36 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { observer } from "mobx-react";
 
 import { StoreCart } from "../store/store";
-
-import { itemCartDto } from "../types/item-cart/itemCart.types";
-import { userDto } from "../types/user/user.types";
 
 import CartItem from "../components/CartItem";
 
 
 const ShoppingCart = observer(() => {
-    const store = useContext(StoreCart)
-
-    const [items, setItems] = useState<itemCartDto[]>();
-    const [user, setUser] = useState<userDto>();
-    const [loading, setLoading] = useState(false);
+    const store = useContext(StoreCart);
 
     // Загружаем корзину при маунте
     useEffect(() => {
         store.fetchCart()
-    }, []); //! ругается на пустые [], @ts-ignore, но мне кажется не стоит так
+    }, [store]); //! ругается на пустые [], @ts-ignore, но мне кажется не стоит так
 
-    // Считаем общую сумму
-    const total = items?.reduce((sum, item) => {
-        return sum + (item.price * item.quantity)
-    }, 0)
-
-    // Обновляем количество товара
-    const updateQuantity = (itemId: number, newQuantity: number) => { //* Обновил типы входных параметров
-        setItems(items?.map(item => item.id === itemId ? { ...item, quantity: newQuantity } : item));
-    }
-
-    // Удаляем товар
-    const removeItem = (itemId: number) => { //* Добавил тип входного значения
-        setItems(items?.filter(item => item.id !== itemId))
-    }
-
-    return loading ?
+    return store.loading ?
         <div>Загрузка...</div>
         :
         <div>
-            <h2>Корзина пользователя {user?.name}</h2>
-            <div>Товаров: {items?.length}</div>
+            <h2>Корзина пользователя {store.user?.name}</h2>
+            <div>Товаров: {store.cartItems.length}</div>
 
             {store.cartItems?.map(item => (
                 <CartItem
                     key={item.id}
                     item={item}
-                    onUpdateQuantity={(qty) => updateQuantity(item.id, qty)}
-                    onRemove={() => removeItem(item.id)}
+                    onUpdateQuantity={(qty) => store.updateQuantity(item.id, qty)}
+                    onRemove={() => store.removeItemCart(item.id)}
                 />
             ))}
 
-            <div>Итого: {total ? total.toFixed(2) : 0} ₽</div>
+            <div>Итого: {store.sumPriceCart} ₽</div>
             <button
                 onClick={() => {
                     console.log('Оформить заказ'); //! Интересная ситуация, в консоль ничего не выводится, а alert сработал!
--- src/store/store.ts
+++ src/store/store.ts
@@ -6,12 +6,14 @@ import { userDto } from '../types/user/user.types';
 import { itemCartDto } from '../types/item-cart/itemCart.types';
 
 class CartItem {
+    id: number;
     name: string;
     price: number;
     quantity: number;
 
-    constructor(name: string, price: number, quantity: number) {
+    constructor(id: number, name: string, price: number, quantity: number) {
         makeAutoObservable(this);
+        this.id = id;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
@@ -22,6 +24,7 @@ class Cart {
     user: userDto | null = null;
     cartItems: itemCartDto[] = [];
     loading = true;
+    sumPriceCart = 0;
 
     constructor() {
         makeAutoObservable(this);
@@ -33,11 +36,38 @@ class Cart {
                 this.loading = false;
                 this.user = response.data.user;
                 this.cartItems = response.data.items;
+                this.totalSum();
             })
             .catch((e) => {
                 console.log(e);
                 this.loading = true;
             })
+    }
+
+    totalSum() {
+        this.sumPriceCart = this.cartItems.reduce((sum, item) => {
+            return sum + (item.price * item.quantity)
+        }, 0)
+    }
+
+    updateQuantity(itemId: number, newQuantity: number) {
+        const indexElChange = this.cartItems.findIndex((item) => item.id === itemId);
+
+        const element = new CartItem(
+            this.cartItems[indexElChange].id,
+            this.cartItems[indexElChange].name,
+            this.cartItems[indexElChange].price,
+            newQuantity
+        );
+
+        this.cartItems[indexElChange] = element;
+
+        this.totalSum();
+    }
+
+    removeItemCart(idItem: number) {
+        this.cartItems = this.cartItems.filter(item => item.id !== idItem);
+        this.totalSum();
     }
 }
 
